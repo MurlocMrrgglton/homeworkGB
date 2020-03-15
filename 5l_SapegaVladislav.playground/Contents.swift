28434d54ad2e71ef29a678522589edf8424f1eda
Vladislav <43825581+MurlocMrrgglton@users.noreply.github.com>
2020-03-15 17:01:37 +0300

    changes

--- 5l_SapegaVladislav.playground/Contents.swift
+++ 5l_SapegaVladislav.playground/Contents.swift
@@ -6,10 +6,6 @@ enum Action {
     case openWindows
 }
 
-enum TrailerState {
-    case connectedTrailer
-    case disconnectedTrailer
-}
 protocol Car {
     var brand: String { get }
     var price: Int { get }
@@ -17,7 +13,7 @@ protocol Car {
     var engine: Action { get set }
     var window: Action { get set }
     
-    func changeState()
+    mutating func changeState(_ action: Action)
 }
 // 2. Создать расширения для протокола «Car» и реализовать в них методы конкретных действий с автомобилем: открыть/закрыть окно, запустить/заглушить двигатель и т.д. (по одному методу на действие, реализовывать следует только те действия, реализация которых общая для всех автомобилей).
 extension Car {
@@ -35,7 +31,12 @@ extension Car {
 }
 // 3. Создать два класса, имплементирующих протокол «Car» - trunkCar и sportСar. Описать в них свойства, отличающиеся для спортивного автомобиля и цистерны.
 final class TrunkCar: Car {
-
+    
+    enum TrailerState {
+        case connectedTrailer
+        case disconnectedTrailer
+    }
+    
     var brand: String
     var price: Int
     var color: String
@@ -43,12 +44,13 @@ final class TrunkCar: Car {
     var engine: Action
     var trailer: TrailerState
     
-    init(brand: String, price: Int, color: String, engine: Action, window: Action, trailer: TrailerState) {
+    init(brand: String, price: Int, color: String, window: Action, engine: Action, trailer: TrailerState) {
         self.brand = brand
         self.price = price
         self.color = color
-        self.engine = engine
         self.window = window
+        self.engine = engine
+        self.trailer = trailer
     }
     
     func truckTrailer() {
@@ -60,24 +62,82 @@ final class TrunkCar: Car {
     }
 }
 
-// 5. Создать несколько объектов каждого класса. Применить к ним различные действия.
-var trunkCar = TrunkCar(brand: "VW", price: 1000, color: "Black", engine: .startEngine, window: .openWindows, trailer: .connectedTrailer)
-trunkCar.truckTrailer()
-trunkCar.changeState(.closeWindows)
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return ("""
+            brand: \(brand)
+            price: \(price)
+            color: \(color)
+            window: \(window)
+            engine: \(engine)
+            trailer: \(trailer)-
+            \n
+            """)
+    }
+}
 
 
-class sportСar: Car {
+class SportСar: Car {
+    
+    enum Fuel {
+        case diesel
+        case patrol
+        case eco
+    }
+    
+    enum TurboMode {
+        case on
+        case off
+    }
     var brand: String
     var price: Int
     var color: String
     var window: Action
     var engine: Action
+    var fuelType: Fuel
+    var turbo: TurboMode
     
-    init(brand: String, price: Int, color: String) {
+    
+    init(brand: String, price: Int, color: String, window: Action, engine: Action, fuelType: Fuel, turbo: TurboMode) {
         self.brand = brand
         self.price = price
         self.color = color
+        self.window = window
+        self.engine = engine
+        self.fuelType = fuelType
+        self.turbo = turbo
+    }
+    
+    func changeTurboMode() {
+        if turbo == .off {
+            turbo = .on
+        } else {
+            turbo = .off
+        }
     }
 }
+
+extension SportСar: CustomStringConvertible {
+var description: String {
+    return ("""
+        brand: \(brand)
+        price: \(price)
+        color: \(color)
+        window: \(window)
+        engine: \(engine)
+        fuelType: \(fuelType)
+        turbo: \(turbo)-
+        \n
+        """)
+}
+}
+    
+// 5. Создать несколько объектов каждого класса. Применить к ним различные действия.
+var trunkCar = TrunkCar(brand: "VW", price: 1000, color: "Black", window: .openWindows, engine: .startEngine, trailer: .connectedTrailer)
+trunkCar.truckTrailer()
+trunkCar.changeState(.closeWindows)
+var sportCar = SportСar(brand: "BMW", price: 10000, color: "Green", window: .closeWindows, engine: .stopEngine, fuelType: .diesel, turbo: .off)
+sportCar.changeTurboMode()
+sportCar.changeState(.startEngine)
 
 
